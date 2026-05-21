a05e0fae7acdddcb29cad871e718d97fbee2e207
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2026-05-21 13:04:04 +0200

    init: tables init

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 import { HttpService, CategoryID } from './api/http.service'
-import type * as HttpInterfaces from './types/TSTypes';
+import * as HttpInterfaces from './types/TSTypes';
+
+import tablesHTML from "../src/components/tables.html?raw"
+
+const myApp: HTMLElement | null = document.querySelector("#app");
 
 document.body.onload = initialize;
 
-function initialize(): void
+async function initialize(): Promise<void>
 {
 
-  tryUpdateCache();
-
-  
-
-  const myApp: HTMLElement | null = document.querySelector("#app");
+  await tryUpdateCache();
 
   if (!myApp)
     throw new Error("Could not find #app div element!");
 
-  //myApp.className =  "bg-light";
+  myApp.className =  "bg-light";
 
   myApp!.innerHTML =
   `
-    <div class="position-absolute top-50 start-50 translate-middle text-center w-75">
-      <h2 class="fw-light">A weboldal még fejlesztés alatt áll...</h2>
-      <h4 class="fst-italic">Térj vissza később!</h4>
-    </div>
+    ${tablesHTML}
   `;
+
+  //console.log(await HttpService.deleteEntry('1', CategoryID.IPlayers, "players"));
+
+  createTableModalTeams();
+
+  listenForTableModal();
 }
 
 async function tryUpdateCache(): Promise<void>
@@ -46,4 +49,66 @@ async function tryUpdateCache(): Promise<void>
     let myError = err as Error;
     alert(myError.message);
   }
+}
+
+function listenForTableModal(): void
+{
+  const form: HTMLFormElement | null = document.querySelector("#table-form-modal");
+
+  if (!form)
+    throw new Error("The form was not found!");
+
+  form.addEventListener("submit", (ev: SubmitEvent) =>
+  {
+    ev.preventDefault();
+
+    const data: FormData = new FormData(form);
+
+    console.log(Array.from(data.values()));
+
+    form.reset();
+
+  });
+}
+
+function createTableModalTeams(): void
+{
+  const divModalTeam: HTMLElement | null = myApp!.querySelector("#team-body");
+
+  if (!divModalTeam)
+    return;
+
+  HttpService.getTeams.forEach(team =>
+  {
+    divModalTeam.appendChild(
+      createTeamInput(team)
+    );
+  })
+}
+
+function createTeamInput(team: HttpInterfaces.ITeam): HTMLElement
+{
+
+  const parent: HTMLElement = document.createElement("div");
+  parent.className = "form-check";
+
+  const teamIDValue: string = team.id!.toString();
+
+  const checkbox: HTMLInputElement = document.createElement("input");
+  const checkboxID: string = `csapat_id_${teamIDValue}`;
+  checkbox.className = "form-check-input";
+  checkbox.type = "checkbox";
+  checkbox.id = checkboxID;
+  checkbox.value = teamIDValue;
+
+  parent.appendChild(checkbox);
+
+  const teamLabel: HTMLLabelElement = document.createElement("label");
+  teamLabel.className = "form-check-label";
+  teamLabel.setAttribute("for", checkboxID);
+  teamLabel.innerText = team.name;
+
+  parent.appendChild(teamLabel);
+
+  return parent;
 }

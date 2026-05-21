f1dbc721b7a8167477d3a5202d2b36f21a620704
Pintér Gábor <pinter.gabor@students.jedlik.eu>
2026-05-21 12:56:42 +0200

    20260521 | staches commit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import '/styles/style.css?url'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
+<<<<<<< HEAD
 import { HttpService } from './api/http.service'
 
 type ViewInit = (appDiv: HTMLElement, callback: Function) => void;
 
 const myApp: HTMLElement | null = document.querySelector("#app");
+=======
+import { HttpService, CategoryID } from './api/http.service'
+import * as HttpInterfaces from './types/TSTypes';
+>>>>>>> e66783a (20260521 | staches commit)
 
 document.body.onload = initialize;
 
@@ -19,9 +24,24 @@ async function initialize(): Promise<void>
   }
 
   await tryUpdateCache();
+<<<<<<< HEAD
+=======
+  
+  
 
+>>>>>>> e66783a (20260521 | staches commit)
+
+  displayPlayers();
+
+
+
+
+
+  const myApp: HTMLElement | null = document.querySelector("#app");
+  
   if (!myApp)
     throw new Error("Could not find #app div element!");
+<<<<<<< HEAD
   myApp.className =  "bg-light";
   
   await loadView('main', myApp);
@@ -54,6 +74,15 @@ async function loadView(viewName: string, parent: HTMLElement): Promise<void>
     console.error(err.message);
     myApp.innerHTML = '<div class="alert alert-danger">Az oldal betöltése sikertelen volt. F12 több információért!</div>';
   }
+=======
+  
+  //myApp.className =  "bg-light";
+  
+  window.addEventListener("cachechanged", () => {
+    displayPlayers();
+  });
+
+>>>>>>> e66783a (20260521 | staches commit)
 }
 
 async function tryUpdateCache(): Promise<void>
@@ -72,4 +101,70 @@ async function tryUpdateCache(): Promise<void>
     let myError = err as Error;
     alert(myError.message);
   }
+}
+
+async function displayPlayers() {
+
+  const playerHTML = (await import('../src/components/players.html?raw')).default;
+  const myApp: HTMLElement | null = document.querySelector("#app");
+  const tabContent = document.createElement("div");
+  const table = document.createElement("table");
+  table.id = "playersTable"
+  const teams = HttpService.getTeams;
+
+  myApp!.innerHTML = playerHTML;
+
+  tabContent.classList += "tab-content";
+  tabContent.id = "myTabContent";
+
+  table.classList += `table`;
+  table.innerHTML += `
+    <thead>
+        <th>#</th>
+        <th>Név</th>
+        <th>Csapata</th>
+        <th>Pozíció</th>
+        <th>Műveletek</th>
+    </thead>
+  `;
+  HttpService.getPlayers.forEach(p => {
+    
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${p.id}</td>
+    <td>${p.name}</td>
+    <td>${getTeamById(p.teamID)}</td>
+    <td>${p.position}</td>
+    <td>
+    <button class = 'btn btn-update' data-id=${p.id}>Szerkesztés</button>
+    <button class = 'btn btn-delete' data-id=${p.id}>Törlés</button>
+    </td>
+    `;
+    table.appendChild(tr);
+  });
+  tabContent?.appendChild(table);
+  myApp?.appendChild(tabContent);
+
+  document.querySelector("#playersTable")?.addEventListener("click", async (e) => {
+    let btn = e.target as HTMLButtonElement;
+    let id = Number.parseInt(btn.dataset.id!);
+    if(btn.classList.contains("btn-delete")){
+      console.log(id);
+      
+      console.log(await HttpService.deleteEntry(id, CategoryID.IPlayers, "players"));
+      
+    }
+  });
+
+}
+
+function getTeamById(id: string) : string{
+  let return_val = "Nincs csapata";
+  HttpService.getTeams.forEach(t => {
+    if(t.id?.toString() == id){
+      return_val = t.name;
+    }
+    
+  });
+  return return_val;
 }
